9f00790fef616bfc3fa04fb771dd4d1288b7cd65
Hatice Albayrak <haticekbr17@gmail.com>
2023-03-16 23:12:04 +0300

    Data Access Layer da mimari çalışmaları devam etti.

---
+++ DataAccessLayer/Concrete/Repositories/LanguageRepository.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class LanguageRepository : ILanguageDAL
+    {
+        Context c = new Context();
+       DbSet<Language> _object;
+
+        public void Delete(Language p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Language p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Language> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Language p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+//ToList
+//Add
+//Remove
+//Find
